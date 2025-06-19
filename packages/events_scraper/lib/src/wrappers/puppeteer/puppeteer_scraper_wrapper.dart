76fb91a8b1fb09611bd3dddb2235f9a1a470fbb3
Karlo M <karlo.marinovic@decode.agency>
2025-06-19 17:07:21 +0200

    sad

--- packages/events_scraper/lib/src/wrappers/puppeteer/puppeteer_scraper_wrapper.dart
+++ packages/events_scraper/lib/src/wrappers/puppeteer/puppeteer_scraper_wrapper.dart
@@ -16,7 +16,8 @@ mixin _PuppeteerScraperWrapperMixin {
   Future<Browser> getBrowser() async {
     final Browser browser = await puppeteer.launch(
       // NOTE: Not working on linux, or in docker. "args" works tho
-      // headless: true,
+      headless: true,
+      devTools: false,
       // noSandboxFlag: true,
       args: [
         // list of flags that worked - # https://github.com/puppeteer/puppeteer/issues/11028
@@ -25,17 +26,17 @@ mixin _PuppeteerScraperWrapperMixin {
         // '--headless',
         /////////////////////
         // new try
-        // '--use-fake-device-for-media-stream',
-        // '--no-zygote',
-        // '--allow-file-access-from-files',
+        '--use-fake-device-for-media-stream',
+        '--no-zygote',
+        '--allow-file-access-from-files',
         '--no-sandbox',
         '--headless',
-        // '--disable-web-security',
-        // '--disable-features=IsolateOrigins',
-        // '--disable-site-isolation-trials',
-        // '--disable-gpu',
-        // '--disable-dev-shm-usage',
-        // '--disk-cache-dir=/dev/null',
+        '--disable-web-security',
+        '--disable-features=IsolateOrigins',
+        '--disable-site-isolation-trials',
+        '--disable-gpu',
+        '--disable-dev-shm-usage',
+        '--disk-cache-dir=/dev/null',
       ],
     );
 
